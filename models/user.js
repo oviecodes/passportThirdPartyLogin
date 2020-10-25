e701b8246da57a03f2625213d48566d7c37b6e13
Godwin Alexander <alecgee73@gmail.com>
2020-10-25 04:15:37 +0100

    create user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+
+
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+})
+
+userSchema.pre('save', async function() {
+    return new Promise((resolve, reject) => {
+       try {
+            const salt = await bcrypt.genSalt(10)
+            resolve(this.password = await bcrypt.hash(this.password, salt))
+       } catch (error) {
+           reject(new Error(error))
+       }
+        
+    })
+})
+
+userSchema.methods.validPassword = async function(password){
+    return new Promise((resolve, reject) => { 
+        try {
+            resolve(await bcrypt.compare(password, this.password))
+        } catch (error) {
+           reject(new Error(error)) 
+        }
+        
+    })
+}
+
+module.exports = mongoose.model('user', userSchema)
